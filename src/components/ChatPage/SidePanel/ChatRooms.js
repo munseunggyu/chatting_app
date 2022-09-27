eec02ec91d2fd3e8ebaa4d6ad0954196127b0e9c
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2022-09-28 01:13:47 +0900

    Add 공개 채팅방 생성 in ChatRooms

--- src/components/ChatPage/SidePanel/ChatRooms.js
+++ src/components/ChatPage/SidePanel/ChatRooms.js
@@ -3,10 +3,47 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import { useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { child, getDatabase, push, ref, update } from 'firebase/database';
+import app from '../../../firebase';
 function ChatRooms(){
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const user = useSelector(state => state.user.currentUser)
+
+  
+  
+  // 채팅방 생성시 들어가는 데이터
+  const [roomName,setRoomName] = useState('')
+  const [roomDes,setRoomDes] = useState('')
+  const addChatRoom = async () => {
+  // 시간 순으로 자동으로 정렬된다. 자동으로 해당 방의 키값 생성
+
+    const chatRoomKey = push(child(ref(getDatabase(app)), 'chatRooms')).key;
+    const newChatRoom = {
+      id:chatRoomKey,
+      name:roomName,
+      description:roomDes,
+      CreateBy:{
+        name:user.displayName,
+        image:user.photoURL
+      }
+    }
+    try{
+      await update(ref(getDatabase(app),`chatRooms/${chatRoomKey}`),newChatRoom)
+    }catch(error){
+      console.log(error)
+    }
+  }
+  const handleSubmit = (e) => { // 생성 버튼 클릭시 실행
+    e.preventDefault()
+    if(roomName && roomDes){
+      addChatRoom()
+      handleClose()
+    }
+  }
+  
   return(
     <div>
       <div style={{position:'relative',with:'100%'
@@ -26,10 +63,11 @@ function ChatRooms(){
           <Modal.Title>Create a chat room</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+          <Form onClick={handleSubmit}>
             <Form.Group className="mb-3" controlId="formRoomName">
               <Form.Label>방 이름</Form.Label>
               <Form.Control
+                onChange={(e) => setRoomName(e.target.value)}
                 type="text"
                 placeholder='Enter a chat room name'
                 autoFocus
@@ -40,16 +78,18 @@ function ChatRooms(){
               controlId="formRoomDescription"
             >
               <Form.Label>방 설명</Form.Label>
-              <Form.Control type='text' placeholder='Enter a chat room description'/>
+              <Form.Control 
+              onChange={(e) => setRoomDes(e.target.value)}
+              type='text' placeholder='Enter a chat room description'/>
             </Form.Group>
           </Form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
-            Close
+            닫기
           </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Create
+          <Button variant="primary" onClick={handleSubmit}>
+            방생성
           </Button>
         </Modal.Footer>
       </Modal>
