c242d5db42509ab0ea2495c4147eab2861845c24
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2022-09-29 09:49:32 +0900

    Change handleClick 함수 변경

--- src/components/ChatPage/MainPanel/MessageForm.js
+++ src/components/ChatPage/MainPanel/MessageForm.js
@@ -1,10 +1,11 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { addDoc, collection, doc, setDoc } from 'firebase/firestore';
+import { addDoc, collection, collectionGroup, orderBy, getDocs, query, QuerySnapshot, setDoc, where } from 'firebase/firestore';
 import { db } from '../../../firebase';
 import { useSelector } from 'react-redux';
+import { async } from '@firebase/util';
 
 
 function MessageForm() {
@@ -17,27 +18,43 @@ function MessageForm() {
       alert('방을 선택해 주세요')
       return
     }
-    /**
-     * message라는 선택된 채팅방의 아이디를 가진 컬렉션을 추가합니다
-     * 컬렉션을 추가할때 아이디값을 지정해서 만듭니다
-     */
-    const ChatRoomRef = doc(db,'message',currentChatId.currentChatRoom.id)
-    /**
-     * 컬렉션을 추가 할때 랜덤한 아이디를 생성해 줍니다
-     * @params ChatRoomRef 위에서 만든 message 컬렉션과 연결합니다
-     * @params 선택된 채팅방의 이름으로 message 컬렉션 하위요소로 추가됩니다
-     */
-    const ChatRoomName = doc(collection(ChatRoomRef,currentChatId.currentChatRoom.name))
-  
-    await setDoc(ChatRoomName,{content,id:ChatRoomName.id}) // 문서를 추가 합니다
+    
+    const citiesRef = collection(db, 'message2');
+    const newId = collection(citiesRef, currentChatId.currentChatRoom.id, currentChatId.currentChatRoom.name)
+    await Promise.all([
+        addDoc(newId, {
+            name: content,
+            id:currentChatId.currentChatRoom.id,
+            CreateAt:new Date()
+          }),
+    
+    ]);
   }
 
   const handleSubmit = (e) => {
     e.preventDefault()
     handleClick()
+    setContent('')
     console.log('완료')
   }
-    return (
+
+  // 데이터 가져오기
+  const getMessageData = async () => {
+    if(currentChatId.currentChatRoom === null){
+      return
+    }
+    const museums = query(collectionGroup (db, currentChatId.currentChatRoom.name), where('id', '==', currentChatId.currentChatRoom.id))
+    if(!museums) return
+    const querySnapshot = await getDocs(museums);
+    querySnapshot.forEach((doc) => {
+       console.log(doc)
+    });
+  }
+  useEffect(() => {
+    getMessageData()
+  },[currentChatId])
+
+  return (
     <div>
     <Form onSubmit={handleSubmit}>
     <Form.Group controlId="exampleForm.ControlTextarea1">
@@ -79,7 +96,7 @@ function MessageForm() {
     />
 
     </div>
-)
+  )
 }
 
 export default MessageForm
